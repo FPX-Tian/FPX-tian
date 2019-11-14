ba5026c252175a00167211f1bc46722da107e536
dsakjbnhuijfhalkfuhjalisfhsaik <57608779+dsakjbnhuijfhalkfuhjalisfhsaik@users.noreply.github.com>
2019-11-14 11:25:06 +0800

    Delete Test.java

--- deom/src/main/java/test/Test.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("122");
-        System.out.println("111");
-        System.out.println("ss");
-    }
-}
